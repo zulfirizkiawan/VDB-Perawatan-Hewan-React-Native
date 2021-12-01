abe267acdd293b53bbcdaa9b6696bfb33b5d6783
zulfirizkiawan <zulfi.rizkiawan.27@gmail.com>
2021-12-01 16:29:49 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>hhhajaj</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
